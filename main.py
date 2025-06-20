69283ec3048557c7bfb45ff632712b992f810f5a
Cute Boy <abdoulazizc867@gmail.com>
2025-06-20 01:10:51 +0000

    Conter exercise

--- main.py
+++ main.py
@@ -2,13 +2,24 @@
 
 from kivy.app import App
 from kivy.metrics import dp
+from kivy.properties import StringProperty
 from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
 #from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 
+
+class WidgetsExemple(GridLayout):
+    my_text = StringProperty("Hey !")
+    conter = 0
+    def on_button_click(self):
+        print("Button click")
+        # self.my_text = "Hello"
+        self.conter = self.conter + 1
+        self.my_text = str(self.conter)
 
 class MainWidget(Widget):
     pass
